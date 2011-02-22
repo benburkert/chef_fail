2b05041b2e6fd7365ab70361fa94e32f166c8f0a
Ben Burkert <ben@benburkert.com>
2011-02-22 15:25:16 -0800

    Add comments.

--- cookbooks/main/definitions/outer.rb
+++ cookbooks/main/definitions/outer.rb
@@ -2,14 +2,14 @@ define :outer do
   _foo = params[:foo] || raise("foo is nil")
 
   execute 'outer test' do
-    command "[[ ! -s #{params[:foo]} ]]"
+    command "[[ ! -s #{params[:foo]} ]]" # params is in scope, parasm[:foo] has a value
   end
 
   inner 'explicit' do
-    foo _foo
+    foo _foo # _foo is in scope, so it has a value
   end
 
   inner 'implicit' do
-    foo params[:foo]
+    foo params[:foo] # params is from a different scope, so params[:foo] has no value
   end
 end
